1f8e786dff2ae14dc2cfe082074c9ef785904c47
Lernt33 <giorgizautashvili24@gmail.com>
2024-07-22 20:29:59 +0400

    doin  doin doin

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,13 +3,15 @@ nav_dropdown_background = document.querySelector('.nav-rest');
 nav_elements.forEach(el => {
     el.addEventListener('focus', (e) => {
         nav_dropdown_background.style.display = 'block';
+        nav_dropdown_background.classList.remove('hidden-element');
         document.getElementById(`nav-dropcontent-${el.attributes[1].value}`).style.display = 'flex';
     })
 })
 nav_elements.forEach(el => {
     el.addEventListener('focusout', (e) => {
-            nav_dropdown_background.style.display = 'none';
-            document.getElementById(`nav-dropcontent-${el.attributes[1].value}`).style.display = 'none';
+        // nav_dropdown_background.style.display = 'none';
+        nav_dropdown_background.classList.add('hidden-element');
+        document.getElementById(`nav-dropcontent-${el.attributes[1].value}`).style.display = 'none';
     })
 
 })
