0ca6943429b72492757d2cbe795c482b3e798fe3
Lernt33 <giorgizautashvili24@gmail.com>
2024-07-28 23:25:38 +0400

    A bit fix

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -42,7 +42,7 @@ document.querySelector('.burger').addEventListener('click', function () {
     if (navRestMobile.classList.length < 3) {
         setTimeout(() => {
             navRestMobile.style.display = 'none'
-        }, 600)
+        }, 200)
 
     } else {
         setTimeout(() => {
