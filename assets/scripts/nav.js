08a557d02a36a7ed6bca2a80ecfb804d447a7c0e
Lernt33 <giorgizautashvili24@gmail.com>
2024-07-28 15:34:00 +0400

    Almost done

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -2,9 +2,7 @@ const nav_elements = document.querySelectorAll('.nav-drop-element');
 const nav_dropdown_background = document.querySelector('.nav-rest');
 const drop_contents = document.querySelector('.dropdown-content')
 const config = {
-    1: false,
-    2: false,
-    3: false,
+    1: false, 2: false, 3: false,
 }
 nav_elements.forEach(el => {
     el.addEventListener('focus', (e) => {
@@ -40,35 +38,40 @@ document.querySelector('.burger').addEventListener('click', function () {
     const navRestMobile = document.querySelector('.nav-rest-mobile');
     if (navRestMobile.style.display === 'none') {
         navRestMobile.style.display = 'block';
-    } else {
+    }
+    if (navRestMobile.classList.length < 3) {
         setTimeout(() => {
             navRestMobile.style.display = 'none'
         }, 600)
+
+    } else {
+        setTimeout(() => {
+            navRestMobile.style.display = 'block'
+        }, 600)
     }
-    navRestMobile.classList.toggle('hidden-element')
-    ;
+    navRestMobile.classList.toggle('hidden-element');
 
 })
 
 document.querySelectorAll('.dropdown-arroy-image').forEach(el => {
-    el.parentElement.addEventListener('click', () => {
+    el.parentElement.parentElement.addEventListener('click', () => {
         // turnign off all dropdowns
         document.querySelectorAll('.dropdown-box').forEach(el2 => {
-            if (el2.querySelector('.reversed') && el.parentElement !== el2) {
+            if (el2.querySelector('.reversed') && el.parentElement.parentElement !== el2) {
                 el2.querySelector('.reversed').classList.remove('reversed')
                 el2.querySelector('.dropdown-active').classList.add('dropdown-hidden')
                 el2.querySelector('.dropdown-active').classList.remove('dropdown-active')
-                el2.classList.remove('increased-height')
+                el2.classList.toggle(`increased-height${el2.attributes[0].value}`)
             }
         })
         el.classList.toggle('reversed');
-        const content = el.parentElement.querySelector('.dropdown-content')
+        const content = el.parentElement.parentElement.querySelector('.dropdown-content')
         if (content.style.display === 'none') {
             content.style.display = 'flex'
         }
         content.classList.toggle('dropdown-active')
         content.classList.toggle('dropdown-hidden')
-        const contentFather = el.parentElement
-        contentFather.classList.toggle('increased-height')
+        const contentFather = el.parentElement.parentElement
+        setTimeout(() => contentFather.classList.toggle(`increased-height${contentFather.attributes[0].value}`), 100)
     })
 })
